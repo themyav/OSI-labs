e7f333e7d9c2fe65731bc7a3417c4a5c732d5fd8
Myav <62132770+themyav@users.noreply.github.com>
2023-09-25 13:08:21 +0300

    Update only-dump.c

--- only-dump.c
+++ only-dump.c
@@ -2,7 +2,7 @@ int dump(void){
 	struct proc *p = myproc();
 	uint64 *s2_ptr = &(p->trapframe->s2);
 	for(int i = 0; i < 10; i++){
-		 printf("s%d = %d\n", i + 2, *(s2_ptr + i));
+		 printf("s%d = %d\n", i + 2, (uint32) *(s2_ptr + i));
 	}
 	return 0;
 }
