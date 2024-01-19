af1655f809c6699e18373f21841f5e3003c03b06
Myav <62132770+themyav@users.noreply.github.com>
2024-01-19 20:33:34 +0300

    Create tcp_client.h

---
+++ Lab4/tcp_client.h
@@ -0,0 +1,33 @@
+
+#ifndef NETWORKFS_TCP
+#define NETWORKFS_TCP
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <net/sock.h>
+#include <linux/inet.h>
+#include <linux/socket.h>
+
+#define MAX_ENTRIES 16
+#define MAX_NAME_LENGTH 256
+
+struct entry {
+  unsigned char entry_type;
+  ino_t ino;
+  char name[MAX_NAME_LENGTH];
+};
+
+struct entries {
+  size_t entries_count;
+  struct entry entries[MAX_ENTRIES];
+};
+
+struct entry_info {
+    unsigned char entry_type;
+    ino_t ino;
+};
+
+int list_call(ino_t inode, struct entries* result);
+int lookup_call(ino_t parent_inode, char* name, struct entry_info* result);
+
+#endif
